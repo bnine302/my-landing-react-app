f25d5888ecf70f10520c547ada1bc5740f61cf98
no-support <bnine19302@gmail.com>
2024-11-19 15:03:09 +0900

    setting: 색상 팔레트 설정

---
+++ src/component/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="bg-gray-44">Footer</div>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: ' rgb(0, 137, 248)',
+        secondary: 'rgb(25, 89, 141)',
+        gray: {
+          44: 'rgb(44, 44, 44)',
+          51: 'rgb(51, 51, 51)',
+          68: 'rgb(68, 68, 68)',
+          102: 'rgb(102, 102, 102)',
+          122: 'rgb(122, 122, 122)',
+          136: 'rgb(136, 136, 136)',
+          204: 'rgb(204, 204, 204)',
+          221: 'rgb(221, 221, 221)',
+          237: 'rgb(237, 237, 237)',
+          241: 'rgb(241, 241, 241)',
+          250: 'rgb(250, 250, 250)',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
