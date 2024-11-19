1342f01faec5efdc1243a0cc3f18fb3f00bb320d
no-support <bnine19302@gmail.com>
2024-11-19 17:13:14 +0900

    fix: footer가 항상 아래에 있도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import Footer from './component/Footer'
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex-shrink-0"></div>
       <Footer />
-    </>
+    </div>
   )
 }
 
