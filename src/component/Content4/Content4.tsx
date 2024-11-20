93d4860592d03312827a8d89a3c3aa7c23b3b491
no-support <bnine19302@gmail.com>
2024-11-20 14:39:21 +0900

    release: 중간 발표를 위한 현재까지의 구현 사항 배포

---
+++ src/component/Content4/Content4.tsx
@@ -0,0 +1,9 @@
+import Section600 from './Section600'
+
+export default function Content4() {
+  return (
+    <div data-id="4">
+      <Section600 />
+    </div>
+  )
+}
---
+++ src/component/Content4/Section600.tsx
@@ -0,0 +1,16 @@
+export default function Section600() {
+  return (
+    <div className="my-section">
+      <div
+        className="
+      my-container"
+      >
+        <h2 className="my-main-title main-title pb-20 text-center ">빅데이터 로그분석 서비스</h2>
+
+        <div className="dataset border-t border-gray-221 pt-20 pb-20">
+          <div className="data-row flex py-20 px-0 odd:flex-row-reverse"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
