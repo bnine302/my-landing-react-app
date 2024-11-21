d79b8cea3439cab8078f99779d76fe5e9b4dd925
no-support <bnine19302@gmail.com>
2024-11-21 14:04:38 +0900

    setting: 백엔드 server.port 값 변경(to 8090)으로 인한 현행화

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // /api/* 요청을 백엔드 서버(http://localhost:8080/api/*)로 전달
+      // /api/* 요청을 백엔드 서버(http://localhost:8090/api/*)로 전달
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8090',
         changeOrigin: true,
         secure: false,
       },
